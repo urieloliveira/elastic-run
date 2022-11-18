3474612ba1b29cd65f38f236d0778ab545a7ee5c
Uriel Guy <urielsateles@gmail.com>
2022-11-18 09:31:17 -0300

    fix: build web app

--- apps/web/src/infra/apm.ts
+++ apps/web/src/infra/apm.ts
@@ -1,6 +1,6 @@
 import { init as apmInit } from "@elastic/apm-rum";
 export default apmInit({
   serviceName: "web-app",
-  serverUrl: "http://apm:8200",
+  serverUrl: "http://localhost:8200",
   environment: "local",
 });
