4629afc3b01617b308e7b6f8cdae97cfc639bc8e
Uriel Guy <urielsateles@gmail.com>
2022-12-06 17:12:21 -0300

    feat: integration prometheus

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,16 @@
 import "dotenv/config";
 import express, { Request, Response } from "express";
 import apm from "elastic-apm-node";
+import promBundle from "express-prom-bundle";
+import { Counter, Gauge, Histogram } from "prom-client";
+
+const metricsMiddleware = promBundle({
+  includeMethod: true,
+  includePath: true,
+  customLabels: {
+    project_version: "2.0",
+  },
+});
 
 apm.start({
   serviceName: process.env.ELASTIC_APM_SERVICE_NAME,
@@ -10,8 +20,33 @@ apm.start({
 
 const app = express();
 
+app.use(metricsMiddleware);
+
+const onlineUsers = new Gauge({
+  name: "node_app_online_users",
+  help: "Online users",
+  labelNames: ["method", "code"],
+});
+
+const httpRequestsTotal = new Counter({
+  name: "node_app_http_requests_total",
+  help: "Total number of HTTP requests made.",
+});
+
+const httpRequestsDuration = new Histogram({
+  name: "node_app_http_requests_duration_seconds",
+  help: "Duration of HTTP requests in seconds.",
+  labelNames: ["method", "code", "handler"],
+});
+
 app.get("/", (_req: Request, res: Response) => {
-  console.log("Teste");
+  onlineUsers.set(
+    { method: "get", code: 200 },
+    parseInt((Math.random() * 100).toString(), 10)
+  );
+  onlineUsers.labels("post", "300").inc();
+  httpRequestsTotal.inc();
+  httpRequestsDuration.labels("get", "200", "index").startTimer()();
   res.send("Hello!");
 });
 
